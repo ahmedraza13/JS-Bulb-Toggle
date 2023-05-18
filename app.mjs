43a2c123f876bb2306bf2dc5cbbb762f84f7ee79
ahmedraza13 <ahmedfaisalakhawala@gmail.com>
2023-05-19 00:25:09 +0500

    bulb assignment

---
+++ app.mjs
@@ -0,0 +1,9 @@
+window.BulbOff = () => {
+  let image = document.querySelector("img");
+  image.src = "./Images/bulboff.gif";
+}
+
+window.BulbOn = () => {
+  let image = document.querySelector("img");
+  image.src = "./Images/bulbon.gif";
+}
